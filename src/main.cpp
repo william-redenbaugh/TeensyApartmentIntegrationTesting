6306c327ecb0ebf221ce9af0d70f9e0ea66511af
= <=>
2020-05-04 22:59:55 -0700

    added some stuff

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,11 @@
 #include "ChRt.h"
 #include "pb.h"
 #include "status.pb.h"
+#include "pb_decode.h"
+#include "pb_encode.h"
+#include "pb.h"
+
+void protobuffer_testing(void);
 
 // LED_BUILTIN pin on Arduino is usually pin 13.
 
@@ -21,6 +26,7 @@ static THD_FUNCTION(Thread1, arg) {
 
     // Turn LED off.
     digitalWrite(LED_BUILTIN, LOW);
+    protobuffer_testing();
   }
 }
 //------------------------------------------------------------------------------
@@ -65,5 +71,26 @@ void setup() {
 //------------------------------------------------------------------------------
 // loop() is the main thread.  Not used in this example.
 void loop() {
+
+}
+
+void protobuffer_testing(void){
+  BoardStatus board_status; 
+  board_status.free_mem_kb = 490; 
+  board_status.status = 1; 
+
+  uint8_t buffer[128];
+  size_t msg_len = sizeof(buffer); 
+
+  pb_ostream_t stream_out = pb_ostream_from_buffer(buffer, sizeof(buffer));
+  pb_encode(&stream_out, BoardStatus_fields, &board_status);
+
+  pb_istream_t stream_in  = pb_istream_from_buffer(buffer, sizeof(buffer));
+  
+  BoardStatus board_status_in; 
+  pb_decode(&stream_in, BoardStatus_fields, &board_status_in);
+
+  Serial.print("Board Free Memory: ");
+  Serial.println(board_status_in.free_mem_kb);
 
 }
