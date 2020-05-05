b47ad3dc3b0fd434fcdc1901a03aad8f6d62b884
= <=>
2020-05-05 02:27:57 -0700

    re arranged testing so that we also use the main thread

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 
 void protobuffer_testing(void);
 
+uint8_t proto_testing_buffer[128];
 // LED_BUILTIN pin on Arduino is usually pin 13.
 
 // Declare a semaphore with an inital counter value of zero.
@@ -16,7 +17,7 @@ SEMAPHORE_DECL(sem, 0);
 // Thread 1, turn the LED off when signalled by thread 2.
 //
 // 64 byte stack beyond task switch and interrupt needs.
-static THD_WORKING_AREA(waThread1, 64);
+static THD_WORKING_AREA(waThread1, 1024);
 
 static THD_FUNCTION(Thread1, arg) {
   (void)arg;
@@ -29,41 +30,18 @@ static THD_FUNCTION(Thread1, arg) {
     protobuffer_testing();
   }
 }
-//------------------------------------------------------------------------------
-// Thread 2, turn the LED on and signal thread 1 to turn the LED off.
-//
-// 64 byte stack beyond task switch and interrupt needs.
-static THD_WORKING_AREA(waThread2, 64);
 
-static THD_FUNCTION(Thread2, arg) {
-  (void)arg;
-  pinMode(LED_BUILTIN, OUTPUT);
-  while (true) {
-    digitalWrite(LED_BUILTIN, HIGH);
-
-    // Sleep for 200 milliseconds.
-    chThdSleepMilliseconds(1000);
-
-    // Signal thread 1 to turn LED off.
-    chSemSignal(&sem);
-
-    // Sleep for 200 milliseconds.
-    chThdSleepMilliseconds(1000);
-  }
-}
 //------------------------------------------------------------------------------
 // continue setup() after chBegin().
 void chSetup() {
   // Start threads.
   chThdCreateStatic(waThread1, sizeof(waThread1),
     NORMALPRIO + 2, Thread1, NULL);
-
-  chThdCreateStatic(waThread2, sizeof(waThread2),
-    NORMALPRIO + 1, Thread2, NULL);
 }
 //------------------------------------------------------------------------------
 void setup() {
   // Initialize OS and then call chSetup.
+  pinMode(LED_BUILTIN, OUTPUT);
   chBegin(chSetup);
   // chBegin() resets stacks and should never return.
   while (true) {}
@@ -71,26 +49,28 @@ void setup() {
 //------------------------------------------------------------------------------
 // loop() is the main thread.  Not used in this example.
 void loop() {
+  
+  chThdSleepSeconds(1);
+  // Packing up the message. 
+  BoardStatus board_status; 
+  board_status.free_mem_kb = 485; 
+  board_status.status = 1; 
+  pb_ostream_t stream_out = pb_ostream_from_buffer(proto_testing_buffer, sizeof(proto_testing_buffer));
+  pb_encode(&stream_out, BoardStatus_fields, &board_status);
+  
+  // Signal thread 1 to turn LED off.
+  chSemSignal(&sem);
 
+  chThdSleepSeconds(1);
 }
 
 void protobuffer_testing(void){
+
+  // Unpacking the message. 
   BoardStatus board_status; 
-  board_status.free_mem_kb = 490; 
-  board_status.status = 1; 
-
-  uint8_t buffer[128];
-  size_t msg_len = sizeof(buffer); 
-
-  pb_ostream_t stream_out = pb_ostream_from_buffer(buffer, sizeof(buffer));
-  pb_encode(&stream_out, BoardStatus_fields, &board_status);
-
-  pb_istream_t stream_in  = pb_istream_from_buffer(buffer, sizeof(buffer));
-  
-  BoardStatus board_status_in; 
-  pb_decode(&stream_in, BoardStatus_fields, &board_status_in);
+  pb_istream_t stream_in  = pb_istream_from_buffer(proto_testing_buffer, sizeof(proto_testing_buffer));
+  pb_decode(&stream_in, BoardStatus_fields, &board_status);
 
   Serial.print("Board Free Memory: ");
-  Serial.println(board_status_in.free_mem_kb);
-
+  Serial.println(board_status.free_mem_kb);
 }
