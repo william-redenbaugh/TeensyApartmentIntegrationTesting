ad5f5926498c3ab4be279e98aee76de85e713572
= <=>
2020-05-06 02:14:46 -0700

    added stuff

--- src/main.cpp
+++ src/main.cpp
@@ -35,8 +35,9 @@ static THD_FUNCTION(Thread1, arg) {
 // continue setup() after chBegin().
 void chSetup() {
   // Start threads.
-  chThdCreateStatic(waThread1, sizeof(waThread1),
-    NORMALPRIO + 2, Thread1, NULL);
+  //chThdCreateStatic(waThread1, sizeof(waThread1),
+  //  NORMALPRIO + 2, Thread1, NULL);
+  Serial.begin(115200);
 }
 //------------------------------------------------------------------------------
 void setup() {
@@ -46,31 +47,26 @@ void setup() {
   // chBegin() resets stacks and should never return.
   while (true) {}
 }
-//------------------------------------------------------------------------------
-// loop() is the main thread.  Not used in this example.
-void loop() {
-  
-  chThdSleepSeconds(1);
-  // Packing up the message. 
-  BoardStatus board_status; 
-  board_status.free_mem_kb = 485; 
-  board_status.status = 1; 
-  pb_ostream_t stream_out = pb_ostream_from_buffer(proto_testing_buffer, sizeof(proto_testing_buffer));
-  pb_encode(&stream_out, BoardStatus_fields, &board_status);
-  
-  // Signal thread 1 to turn LED off.
-  chSemSignal(&sem);
 
-  chThdSleepSeconds(1);
+uint16_t buff_place = 0; 
+void loop() {
+  if(Serial.available() > 0){
+    proto_testing_buffer[buff_place] = Serial.read();
+    buff_place++; 
+
+    // Reset when we get to 127
+    if(buff_place < 127){
+      buff_place = 0; 
+      // Unpacking the message. 
+      BoardStatus board_status; 
+      pb_istream_t stream_in  = pb_istream_from_buffer(proto_testing_buffer, sizeof(proto_testing_buffer));
+      pb_decode(&stream_in, BoardStatus_fields, &board_status);
+
+      Serial.print("Board Free Memory: ");
+      Serial.println(board_status.free_mem_kb);
+    }
+  }  
 }
 
 void protobuffer_testing(void){
-
-  // Unpacking the message. 
-  BoardStatus board_status; 
-  pb_istream_t stream_in  = pb_istream_from_buffer(proto_testing_buffer, sizeof(proto_testing_buffer));
-  pb_decode(&stream_in, BoardStatus_fields, &board_status);
-
-  Serial.print("Board Free Memory: ");
-  Serial.println(board_status.free_mem_kb);
 }
