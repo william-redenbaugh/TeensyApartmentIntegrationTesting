939125c98761bc57a307e1d0dc05bf745968177e
= <=>
2020-05-06 21:00:38 -0700

    added a message data and implemented with the teensy

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,14 @@
 #include "pb_decode.h"
 #include "pb_encode.h"
 #include "pb.h"
+#include "messagedata.pb.h"
 
-void protobuffer_testing(void);
-
-uint8_t protobuffer[12500];
-// LED_BUILTIN pin on Arduino is usually pin 13.
+void testing_message_size_pb(void);
 
 // Declare a semaphore with an inital counter value of zero.
 SEMAPHORE_DECL(sem, 0);
-//------------------------------------------------------------------------------
-// Thread 1, turn the LED off when signalled by thread 2.
-//
-// 64 byte stack beyond task switch and interrupt needs.
-static THD_WORKING_AREA(waThread1, 1024);
 
+static THD_WORKING_AREA(waThread1, 1024);
 static THD_FUNCTION(Thread1, arg) {
   (void)arg;
   while (!chThdShouldTerminateX()) {
@@ -27,45 +21,88 @@ static THD_FUNCTION(Thread1, arg) {
 
     // Turn LED off.
     digitalWrite(LED_BUILTIN, LOW);
-    protobuffer_testing();
+    //protobuffer_testing();
   }
 }
 
-//------------------------------------------------------------------------------
-// continue setup() after chBegin().
 void chSetup() {
   // Start threads.
   //chThdCreateStatic(waThread1, sizeof(waThread1),
   //  NORMALPRIO + 2, Thread1, NULL);
+  
   Serial.begin(115200);
-}
-//------------------------------------------------------------------------------
-void setup() {
+
   // Initialize OS and then call chSetup.
   pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void setup() {
+  // We do all of our setup in here. 
   chBegin(chSetup);
   // chBegin() resets stacks and should never return.
   while (true) {}
 }
 
-uint16_t buff_place = 0; 
 void loop() {
   // Once we have 128 bytes in the array.
-  if(Serial.available() >= 12500){
+  if(Serial.available() >= 32){
+    // Struct that we will store Message data in.
+    MessageData message_data; 
+    
+    // Scoping it out so we don't take up too much stack space. 
+    {
+      uint8_t message_instr_arr[32];
+      for(uint8_t i = 0; i < 32; i++)
+        // Popping off all the oldest 32 bytes. 
+        message_instr_arr[i] = Serial.read();
 
-    // We read them into our buffer. 
-    Serial.readBytes(protobuffer, 12500);
+      // Message unpacking instructions. 
+      pb_istream_t msg_in = pb_istream_from_buffer(message_instr_arr, 32);
+      pb_decode(&msg_in, MessageData_fields, &message_data);
+    }
 
-    // And then we unpack em!
-    BoardStatus board_status; 
-    pb_istream_t stream_in  = pb_istream_from_buffer(protobuffer, sizeof(protobuffer));
-    pb_decode(&stream_in, BoardStatus_fields, &board_status);
+    // Byte array with instruction information.
+    uint8_t message_instr_arr[message_data.message_size];
+    switch(message_data.message_type){
+      // When we receive general instruction data. we do stuff here. 
+      case(MessageData_MessageType_GENERAL_INSTRUCTIONS):
+      break;
 
-    // And here we print out data for debugging. 
-    Serial.print("Board Free Memory: ");
-    Serial.println(board_status.free_mem_kb);
+
+      // When we receive matrix data we. we do other stuff here!
+      case(MessageData_MessageType_MATRIX_DATA):
+      break;
+
+      // if we get here. then we fucked up lol. 
+      default:
+      // Reads through whatever is left and clears the buffer. 
+      while(Serial.available())
+        Serial.read();
+      break;
+    }
   }  
+
+  chThdSleepSeconds(1);
+  testing_message_size_pb();
 }
 
-void protobuffer_testing(void){
+void testing_message_size_pb(void){
+    MessageData message_data_out;
+    message_data_out.message_size = 85785967;
+    message_data_out.message_type = MessageData_MessageType_MATRIX_DATA;
+
+    // Put data into serialized format. 
+    uint8_t buffer[32];
+    pb_ostream_t msg_out = pb_ostream_from_buffer(buffer, sizeof(buffer));
+    pb_encode(&msg_out, MessageData_fields, &message_data_out);
+
+    // Unpack serialsed data. 
+    pb_istream_t msg_in = pb_istream_from_buffer(buffer, 32);
+
+    MessageData message_data_in; 
+    pb_decode(&msg_in, MessageData_fields, &message_data_in);
+
+    if(message_data_in.message_size == message_data_out.message_size && message_data_in.message_type == message_data_out.message_type){
+      Serial.println("Protobuffer messages succeeded! :0 ");   
+    }
 }
