b45dec0f9de780b38ed3822d66ebaf092fb53502
= <=>
2020-05-08 15:29:41 -0700

    added integration for strip and matrix stuff in chibios

---
+++ src/MatrixManagement.hpp
@@ -0,0 +1,11 @@
+#ifndef _MATRIXMANAGEMENT_HPP
+#define _MATRIXMANAGEMENT_HPP
+
+class MatrixManagement{
+    public: 
+        void begin(void);
+        void run(void);
+    private: 
+};
+
+#endif 
